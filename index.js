dfbf5924ebd2f6fe6599b99950ec4766add8c8a7
Felix Weinberger <felix.weinberger@gmail.com>
2019-03-07 20:48:41 +0100

    bla

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 function multipliable(a, b, result) {
-  // Go, go, go!
+  const left = a.split('');
+  const right = b.split('');
+
+  const current = {};
+  left.forEach(letter => current[letter] = 0);
+  right.forEach(letter => current[letter] = 0);
+
+  let digits = Object.keys(current).length;
+
+  for (let i = 0; i <= 10 ** (digits) - 1; i++) {
+    const currentNums = String(i).padStart(digits, '0').split('');
+
+    Object.keys(current).forEach((letter, i) => current[letter] = currentNums[i]);
+
+    const leftCurr = Number(left.map(letter => current[letter]).join(''));
+    const rightCurr = Number(right.map(letter => current[letter]).join(''));
+
+    if (leftCurr * rightCurr === Number(result)) return true;
+  }
   
-  return a * b === result;
+  return false;
 }
+
+multipliable('AB', 'AB', '100');
 
 module.exports = multipliable;
